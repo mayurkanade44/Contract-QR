031fb2080633e33422740abca62804a6f9cee80f
mayurkanade44 <mayurkanade44@gmail.com>
2022-02-03 15:25:45 +0530

    doc creating added

--- backend/controllers/service.js
+++ backend/controllers/service.js
@@ -1,6 +1,10 @@
 const Service = require("../models/service");
 const Contract = require("../models/contract");
 const QRCode = require("qrcode");
+const PizZip = require("pizzip");
+const Docxtemplater = require("docxtemplater");
+const fs = require("fs");
+const path = require("path");
 
 const getAllService = async (req, res) => {
   try {
@@ -11,6 +15,41 @@ const getAllService = async (req, res) => {
     console.log(error);
   }
 };
+
+const createDoc = async (isValidContract, services) => {
+  const { contractNo, startDate, endDate, billingFrequency, shipToAddress } =
+    isValidContract;
+  const { name, address, nearBy, pincode } = shipToAddress[0];
+  const {frequency, area, service, specialInstruction, preferred} = services
+  const { day, time } = preferred[0]
+  const content = fs.readFileSync(
+    path.resolve(__dirname, "test.docx"),
+    "binary"
+  );
+
+  const zip = new PizZip(content);
+
+  const doc = new Docxtemplater(zip, {
+    paragraphLoop: true,
+    linebreaks: true,
+  });
+
+  doc.render({
+    contractNo: 123,
+    service: "GS",
+  });
+
+  const buf = await doc.getZip().generate({
+    type: "nodebuffer",
+    // compression: DEFLATE adds a compression step.
+    // For a 50MB output document, expect 500ms additional CPU time
+    compression: "DEFLATE",
+  });
+
+  // buf is a nodejs Buffer, you can either write it to a file or res.send it with express for example.
+  fs.writeFileSync(path.resolve(__dirname, "output.docx"), buf);
+};
+
 const createService = async (req, res) => {
   const { contract: contractId } = req.body;
   const isValidContract = await Contract.findOne({ _id: contractId });
@@ -23,11 +62,13 @@ const createService = async (req, res) => {
       const serviceId = await services._id;
       const contractNo = isValidContract.contractNo;
       const name = `${contractNo}${services.frequency}`;
+      console.log(services.preferred);
 
       const stringdata = `Contract Number: ${contractNo},
 
       url: http://localhost:5000/api/contracts/${serviceId}`;
       await QRCode.toFile(`./${name}.png`, stringdata);
+      // createDoc();
     }
     res.status(201).json({ services });
   } catch (error) {
