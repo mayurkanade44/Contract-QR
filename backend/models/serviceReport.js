9b4591f63b42ca644a0dcdc9d76f5b71fd1b3616
mayurkanade44 <mayurkanade44@gmail.com>
2022-05-07 16:59:31 +0530

    service report model added

---
+++ backend/models/serviceReport.js
@@ -0,0 +1,29 @@
+const moongoose = require("mongoose");
+
+const ServiceReportSchema = new moongoose.Schema({
+  contract: {
+    type: String,
+  },
+  serviceName: {
+    type: String,
+  },
+  comments: {
+    type: String,
+  },
+  completion: {
+    type: String,
+  },
+  image: {
+    type: String,
+  },
+  service: {
+    type: moongoose.Types.ObjectId,
+    ref: "Service",
+    required: true,
+  },
+  serviceDate: {
+    type: String,
+  },
+});
+
+module.exports = moongoose.model("ServiceReport", ServiceReportSchema);
