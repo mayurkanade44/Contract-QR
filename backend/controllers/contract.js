52fd43ae4c947fbd68cbf475468bcbcdc696a775
mayurkanade44 <mayurkanade44@gmail.com>
2022-05-23 11:18:27 +0530

    300 contracts fetch limit & pagination changes

--- backend/controllers/contract.js
+++ backend/controllers/contract.js
@@ -27,6 +27,13 @@ const getAllContracts = async (req, res) => {
         },
       }).sort("-createdAt");
     }
+
+    // const page = Number(req.query.page) || 1;
+    // const limit = 5;
+    // const skip = (page - 1) * limit;
+
+    contracts = contracts.slice(0,300)
+
     res.status(200).json({ contracts, len: contracts.length });
   } catch (error) {
     res.status(500).json({ msg: error });
