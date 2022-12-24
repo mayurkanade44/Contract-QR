7e6ae087c283a0b6241799d3165ff2af1c1fc494
mayurkanade44 <mayurkanade44@gmail.com>
2022-12-24 16:02:55 +0530

    backend get service cards added

--- backend/controllers/adminController.js
+++ backend/controllers/adminController.js
@@ -1,4 +1,5 @@
 const Admin = require("../models/admin");
+const ServiceReport = require("../models/serviceReport");
 
 const addValues = async (req, res) => {
   const { addComment } = req.body;
@@ -11,6 +12,30 @@ const allValues = async (req, res) => {
   res.status(200).json({ allValues });
 };
 
-module.exports = { addValues, allValues };
+const serviceCards = async (req, res) => {
+  const { contract } = req.query;
+  try {
+    const cont = await ServiceReport.find({
+      contract: { contract, $options: "i" },
+    }).select("contract serviceName image serviceDate");
+    if (cont.length <= 0)
+      return res.status(404).json({ msg: "No Contract Found" });
+    const cards = [];
 
+    for (let i = cont.length - 1; i >= 0; i--) {
+      if (cards.length > 0 && cont[i].image.length > 0) {
+        let temp = cards.filter(
+          (item) => item.serviceName === cont[i].serviceName
+        );
+        if (temp.length === 0) cards.push(cont[i]);
+      } else {
+        cards.push(cont[i]);
+      }
+    }
+    res.status(200).json({ cards });
+  } catch (error) {
+    console.log(error);
+  }
+};
 
+module.exports = { addValues, allValues, serviceCards };
--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -4,11 +4,13 @@ const { authorizeUser, authenticateUser } = require("../middleware/auth");
 
 const { register, login } = require("../controllers/authController");
 const { feedback } = require("../controllers/service");
+const { serviceCards } = require("../controllers/adminController");
 
 router
   .route("/register")
   .post(authenticateUser, authorizeUser("Admin"), register);
 router.route("/login").post(login);
 router.route("/feedback/:id").post(feedback);
+router.route("/serviceCard").get(serviceCards)
 
 module.exports = router;
